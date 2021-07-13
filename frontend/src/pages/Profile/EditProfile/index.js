484731dc01631c0c045326f0653748ae0630eef9
Bebolor <bolorchimeg.m@gmail.com>
2021-07-13 17:58:25 +0200

    Edit Profile on process

---
+++ frontend/src/pages/Profile/EditProfile/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styled from "styled-components";
+
+
+const Container = styled.div`
+    margin: 0 auto;
+    margin-bottom: 2rem;
+    background-color: black;
+    border-radius: 4px;
+    height: 500px;
+    box-shadow: 1px 1px 20px -5px #AEAEAE;
+    max-width: 1180px;
+    display: flex;
+    position: relative;
+    top: 10px;
+`;
+
+const EditProfile = () => {
+    return <>
+        <Container>
+
+        </Container>
+    </>
+};
+export default EditProfile
