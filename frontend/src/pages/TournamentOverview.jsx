a91fe063626209edb2a9002a4f8bc0686d1f6b93
Jon Strickler <jon.strickler@bluewin.ch>
2021-07-13 21:54:14 +0200

    fix import

--- frontend/src/pages/TournamentOverview.jsx
+++ frontend/src/pages/TournamentOverview.jsx
@@ -3,8 +3,8 @@ import { SectionTitle } from '../styles/section-title';
 import styled from 'styled-components';
 import Soccer from '../assets/svgs/soccer-ball.svg'
 import Map from '../components/Mapbox'
-import Participant from '../components/Participant'
-import Details from '../components/Details'
+import Participant from '../components/Tournament/Participant'
+import Details from '../components/Tournament/Details'
 
 const BaseButton = styled.button `
     border-radius: 30px;
