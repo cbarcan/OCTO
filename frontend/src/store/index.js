9681fb40e8dafb9ffec7c642e54958fb335ef83a
Jon Strickler <jon.strickler@bluewin.ch>
2021-07-16 09:44:00 +0200

    global menu with state

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,12 +2,12 @@ import { applyMiddleware, createStore, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { user } from './reducers/userReducer'
-import { team } from './reducers/teamReducer'
 import { tournament } from './reducers/tournamentReducer'
 
 const middlewares = [thunk];
 const reducers = combineReducers({
     user,
+    tournament
 });
 
 const store = createStore(reducers, composeWithDevTools(
