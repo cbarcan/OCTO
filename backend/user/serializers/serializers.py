334c30b683e01076b35f261eb5aa3f1867d1d3e4
cbarcan <catalin.barcan@ymail.com>
2021-07-17 12:32:03 +0200

    user serializer change

--- backend/user/serializers/serializers.py
+++ backend/user/serializers/serializers.py
@@ -12,10 +12,13 @@ class UserBracketSerializer(serializers.ModelSerializer):
 
 
 class ListUserSerializer(serializers.ModelSerializer):
+    captain_of_teams = ListTeamSerializer(many=True)
     teams = ListTeamSerializer(many=True)
     part_in_tournaments = ListTournamentSerializer(many=True)
+    my_tournaments = ListTournamentSerializer(many=True)
 
     class Meta:
         model = User
-        fields = ["id", "email", "username", "first_name", "last_name", "company", "location", "avatar", "teams", "part_in_tournaments"]
+        fields = ["id", "email", "username", "first_name", "last_name", "company", "location", "avatar", "banner",
+                  "captain_of_teams", "teams", "my_tournaments", "part_in_tournaments"]
         read_only_fields = ["id"]
