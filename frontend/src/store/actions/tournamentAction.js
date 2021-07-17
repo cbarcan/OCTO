3ae03cee6280e0146dd5240f0e6f0fb4e8ae6ac6
Jon Strickler <jon.strickler@bluewin.ch>
2021-07-17 17:27:29 +0200

    add createTournament fetch

--- frontend/src/store/actions/tournamentAction.js
+++ frontend/src/store/actions/tournamentAction.js
@@ -1,3 +1,45 @@
+import Axios from '../../axios';
+
 export const addTournamentData = (id) => {
     return { type: 'TOURNAMENT_ADD_DATA', payload: id };
+};
+
+export const createTournament = (
+    sport,
+    p,
+    name,
+    avatar,
+    no_of_players,
+    format,
+    location,
+    lat,
+    lng,
+    date,
+    description
+) => {
+    const tournamentData = new FormData();    
+    tournamentData.append('sport', sport)
+    tournamentData.append('private', p)
+    tournamentData.append('name', name)
+    tournamentData.append('avatar', avatar)
+    tournamentData.append('no_of_players', no_of_players)
+    tournamentData.append('format', format)
+    tournamentData.append('location', location)
+    tournamentData.append('latitude', lat)
+    tournamentData.append('longitude', lng)
+    tournamentData.append('date', date)
+    tournamentData.append('description', description)
+
+    const url = 'tournament/new/';
+    const auth = 'Bearer ' + localStorage.getItem('userToken');
+    const headers = {
+        headers: { Authorization: auth, 'Content-Type': 'multipart/form-data' },
+    };
+    Axios.post(url, tournamentData, headers)
+        .then((response) => {
+            console.log('Tournament Creation successful.');
+        })
+        .catch((error) => {
+            console.log('Tournament Creation error', error.response.data);
+        });
 };
