a83dc08133441ebf0988d1d03fd793e7797e85a8
cbarcan <catalin.barcan@ymail.com>
2021-07-19 19:02:13 +0200

    creating SE matches OG

--- backend/tournament/views.py
+++ backend/tournament/views.py
@@ -149,6 +149,7 @@ class RetrieveUpdateDestroyTournamentView(RetrieveUpdateDestroyAPIView):
     lookup_field = 'pk'
 
     def patch(self, request, *args, **kwargs):
+
         try:
             if request.data["status"] == "OG":
                 bracket = Bracket.objects.get(tournament=kwargs["pk"])
@@ -185,6 +186,75 @@ class RetrieveUpdateDestroyTournamentView(RetrieveUpdateDestroyAPIView):
                                 match_serializer.is_valid(raise_exception=True)
                                 match_serializer.save(players=[participants[j], participants[len(participants) - 1 - j]])
                             participants = participants_shuffle(participants)
+                elif tournament.format == "SE":
+                    nr_of_rounds = 0
+                    while len(participants) > pow(2, nr_of_rounds):
+                        nr_of_rounds += 1
+                    nr_of_bye = pow(2, nr_of_rounds) - len(participants)
+                    participants_first_round = len(participants) - nr_of_bye
+                    for i in range(nr_of_rounds, 0, -1):
+                        round_data = {"bracket": bracket_serializer.data}
+                        round_serializer = RoundSerializer(data=round_data)
+                        round_serializer.is_valid(raise_exception=True)
+                        round_serializer.save(bracket_id=bracket_serializer.data["id"])
+                        if i == nr_of_rounds:
+                            if nr_of_bye == 0:
+                                for j in range(0, pow(2, i), 2):
+                                    match_data = {"round": round_serializer.data["id"]}
+                                    match_serializer = MatchSerializer(data=match_data)
+                                    match_serializer.is_valid(raise_exception=True)
+                                    match_serializer.save(players=[participants[j], participants[j + 1]])
+                            else:
+                                for j in range(0, participants_first_round, 2):
+                                    match_data = {"round": round_serializer.data["id"]}
+                                    match_serializer = MatchSerializer(data=match_data)
+                                    match_serializer.is_valid(raise_exception=True)
+                                    match_serializer.save(players=[participants[j], participants[j + 1]])
+                                for j in range(0, nr_of_bye):
+                                    match_data = {"round": round_serializer.data["id"]}
+                                    match_serializer = MatchSerializer(data=match_data)
+                                    match_serializer.is_valid(raise_exception=True)
+                                    match_serializer.save()
+                        elif i == nr_of_rounds - 1:
+                            if nr_of_bye == 0:
+                                for j in range(0, pow(2, i), 2):
+                                    match_data = {"round": round_serializer.data["id"],
+                                                  "mock_players": [{"first_name": "Winner", "last_name": ""}, {"first_name": "Winner", "last_name": ""}]}
+                                    match_serializer = MatchSerializer(data=match_data)
+                                    match_serializer.is_valid(raise_exception=True)
+                                    match_serializer.save()
+                            else:
+                                for j in range(0, int(participants_first_round / 4)):
+                                    match_data = {"round": round_serializer.data["id"],
+                                                  "mock_players": [{"first_name": "Winner", "last_name": ""}, {"first_name": "Winner", "last_name": ""}]}
+                                    match_serializer = MatchSerializer(data=match_data)
+                                    match_serializer.is_valid(raise_exception=True)
+                                    match_serializer.save()
+                                if nr_of_bye % 2 == 0:
+                                    for j in range(participants_first_round, len(participants), 2):
+                                        print(j)
+                                        match_data = {"round": round_serializer.data["id"]}
+                                        match_serializer = MatchSerializer(data=match_data)
+                                        match_serializer.is_valid(raise_exception=True)
+                                        match_serializer.save(players=[participants[j], participants[j + 1]])
+                                else:
+                                    match_data = {"round": round_serializer.data["id"],
+                                                  "mock_players": [{"first_name": "Winner", "last_name": ""}, participants[participants_first_round]]}
+                                    match_serializer = MatchSerializer(data=match_data)
+                                    match_serializer.is_valid(raise_exception=True)
+                                    match_serializer.save()
+                                    for j in range(participants_first_round + 1, pow(2, i), 2):
+                                        match_data = {"round": round_serializer.data["id"]}
+                                        match_serializer = MatchSerializer(data=match_data)
+                                        match_serializer.is_valid(raise_exception=True)
+                                        match_serializer.save(players=[participants[j], participants[j + 1]])
+                        else:
+                            for j in range(0, pow(2, i), 2):
+                                match_data = {"round": round_serializer.data["id"],
+                                              "mock_players": [{"first_name": "Winner", "last_name": ""}, {"first_name": "Winner", "last_name": ""}]}
+                                match_serializer = MatchSerializer(data=match_data)
+                                match_serializer.is_valid(raise_exception=True)
+                                match_serializer.save()
             return self.partial_update(request, *args, **kwargs)
         except KeyError:
             return self.partial_update(request, *args, **kwargs)
