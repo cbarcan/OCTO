809b9b842d92cd3ce3c38dd5ae0eb6fe1b16db11
Jon Strickler <jon.strickler@bluewin.ch>
2021-07-21 17:22:55 +0200

    hide double tournaments

--- frontend/src/pages/Profile/CurrentTournament/index.js
+++ frontend/src/pages/Profile/CurrentTournament/index.js
@@ -75,24 +75,33 @@ const Name = styled.p`
 
 const CurrentTournament = (props) => {
 
+    // build map aary to display
+
+
     return <>
         <TournamentDiv>
             <Header>
                 <Name>Current Tournaments </Name>
-                <Label><p>◀</p><p>▶</p></Label>
             </Header>
+            {
+            props.user ?
             <Tournaments>
+
                 {
                     props.user.my_tournaments.map((tournament, index) => (                
                         <TournamentCard key={index} tournament={tournament}/>
                     ))
                 }
                 {
-                    props.user.part_in_tournaments.map((tournament, index) => (                
-                        <TournamentCard key={index} tournament={tournament}/>
-                    ))
-                }
+                    props.user.part_in_tournaments.map((tournament, index) => {
+                        console.log(!props.user.my_tournaments.contains(tournament))
+                        if (!props.user.my_tournaments.contains(tournament)) return <TournamentCard key={index} tournament={tournament}/>
+                        else return null
+                    })
+                } 
             </Tournaments>
+            : null
+            }
         </TournamentDiv>
     </>
 };
