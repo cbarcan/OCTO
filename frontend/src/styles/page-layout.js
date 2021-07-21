1cd003c9c5d4fa7cb5cf8a2bc716c4514aa9658f
Valentina Sulbaran <vjss05@gmail.com>
2021-07-21 14:05:17 +0200

    Fixes

--- frontend/src/styles/page-layout.js
+++ frontend/src/styles/page-layout.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const PageContainer = styled.div`
-  min-height: 100vh;
+  //min-height: 100vh;
   min-width: 100%;
   width: fit-content;
   display: grid;
@@ -12,7 +12,7 @@ export const PageContainer = styled.div`
 `;
 
 export const PageContent = styled.div`
-  min-height: 100%;
+  //min-height: 100%;
   width: 100%;
   display: flex;
   flex-direction: column;
